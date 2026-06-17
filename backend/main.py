076eb5a98b084db83fd58bca513285afcb99c11b
Vineeth Reddy <vineeth.reddy2323@gmail.com>
2026-06-17 12:34:23 -0500

    Add resume upload and parsing capability

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,40 @@ import os
 from dotenv import load_dotenv
 from groq import Groq
 import json 
+from fastapi import UploadFile, File
+from docx import Document
+from pypdf import PdfReader
+
 
 # Load environment variables
 load_dotenv()
 
 with open("profile.json", "r") as f:
     candidate_profile = json.load(f)
+
+def extract_text_from_docx(file_path):
+
+    doc = Document(file_path)
+
+    text = ""
+
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+
+    return text
+
+
+def extract_text_from_pdf(file_path):
+
+    reader = PdfReader(file_path)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text()
+
+    return text
+
 
 # Get API key from .env
 api_key = os.getenv("GROQ_API_KEY")
@@ -38,63 +66,37 @@ def home():
 
 
 # Interview assistant endpoint
-@app.post("/ask")
-def ask_question(req: QuestionRequest):
+@app.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)):
 
     try:
 
-        prompt = f"""
-You are an interview assistant.
+        file_path = f"uploads/{file.filename}"
 
-Answer as this candidate.
+        with open(file_path, "wb") as buffer:
+            buffer.write(await file.read())
 
-Candidate Profile:
+        if file.filename.endswith(".docx"):
+            resume_text = extract_text_from_docx(file_path)
 
-Name:
-{candidate_profile['name']}
+        elif file.filename.endswith(".pdf"):
+            resume_text = extract_text_from_pdf(file_path)
 
-Role:
-{candidate_profile['title']}
-
-Experience:
-{candidate_profile['experience_years']} years
-
-Skills:
-{', '.join(candidate_profile['skills'])}
-
-Responsibilities:
-{', '.join(candidate_profile['current_responsibilities'])}
-
-Projects:
-{', '.join(candidate_profile['projects'])}
-
-Question:
-{req.question}
-
-Instructions:
-- Answer in first person.
-- Do not invent experience.
-- Keep answer interview-ready.
-- Keep answer under 60 seconds.
-- Provide keywords.
-"""
-
-        response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ],
-            temperature=0.2
-        )
+        else:
+            return {
+                "error":
+                "Only PDF and DOCX supported"
+            }
 
         return {
-            "answer": response.choices[0].message.content
+            "message":
+            "Resume uploaded successfully",
+            "characters":
+            len(resume_text)
         }
 
     except Exception as e:
+
         return {
             "error": str(e)
         }
